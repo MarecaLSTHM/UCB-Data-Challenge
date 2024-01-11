afa9ac92de939c442e49f2afb4465a157caa03f7
MarecaLSTHM <marecasitholeuk2@gmail.com>
2024-01-11 14:55:10 +0000

    Grant R access to Github.

--- R/UCB_Test.R
+++ R/UCB_Test.R
@@ -1,1 +1,10 @@
 # I am adding a comment to test if I can commit
+
+install.packages("usethis")
+
+library(usethis)
+
+usethis::use_git_config(
+  user.name = "MarecaLSTHM",
+  user.email = "marecasitholeuk2@gmail.com",
+  github.user = "MarecaLSTHM")
