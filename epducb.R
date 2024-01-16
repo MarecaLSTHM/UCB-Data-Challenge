5723dbca96881edf55ede6e7c0663970ce0f7b7c
MarecaLSTHM <marecasitholeuk2@gmail.com>
2024-01-16 15:16:24 +0000

    Add spaces to code

--- epducb.R
+++ epducb.R
@@ -1,10 +1,13 @@
 library(tidyverse)
 install.packages("sf")
 library(sf)
+
 #data11=biphosphonates data from open prescribing
 data11 <- read_csv("data11.csv")
+
 #Number of regions
 select(data11,name) %>% unique %>% nrow
+
 #head/tail of data
 head(data11)
 tail(data11)
@@ -40,6 +43,7 @@ ggplot(prescription_data_grouped, aes(x = Year, y = Total_Prescriptions, group =
        x = "Year",
        y = "Number of Prescriptions") +
   theme_minimal()
+
 #Note:information shown just for bisphosphonates
 #Trend explainable as 2018 has only data for two months, but generally, drug prescription can be seen to be on a steady decline from peak(2019)
 #Comparatively, another plot should be done including all medications
@@ -48,23 +52,29 @@ ggplot(prescription_data_grouped, aes(x = Year, y = Total_Prescriptions, group =
 
 #Shape file from ONS
 regions <- st_read("NHSER_APR_2021_EN_BFC.shp")
+
 #arrange alphabetically
 regions <- regions%>% arrange(NHSER21NM)
+
 #auto assign region_id(primary key for future merging)
 regions <- regions %>%
   mutate(region_id = row_number())
+
 #total prescriptions per region
 total_prescriptions_by_region <- data11 %>%
   group_by(name) %>%
   summarise(total_prescriptions = sum(y_items)) 
+
 #arrange alphabetically to match ONS format
 total_prescriptions_by_region <- total_prescriptions_by_region%>% arrange(name) 
+
 #auto assign region_id to match ONS
 total_prescriptions_by_region <- total_prescriptions_by_region %>%
   mutate(region_id = row_number())
 
 #merge ONS shapefile and our dataset
 merged_data <- inner_join(regions, total_prescriptions_by_region, by = "region_id")
+
 #Note this map not so important for Q1, just showing how to merge shapefiles and our transformed data
 #Alternatively it can also show us total prescriptions and how it varies by region
 #Simulation done only for bisphosphonates
@@ -77,4 +87,5 @@ ggplot(merged_data) +
   theme_minimal() +
   theme(axis.text = element_blank(),   
         axis.ticks = element_blank())
+
 #We can add labels on the regions too for proper identification
