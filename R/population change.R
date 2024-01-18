fdf0db7e64aa47a665dfd6b6f84ed546fe066b74
leonchy <u3593370@connect.hku.hk>
2024-01-18 15:46:59 +0000

    modified:   .Rhistory 	new file:   R/population change.R 	modified:   data/LABEL THE DATASETS.docx 	new file:   data/population_over_50.csv 	new file:   output/osteoporosis_population_2018_2022.png

--- R/population change.R
+++ R/population change.R
@@ -2,19 +2,27 @@ library(here)
 library(ggplot2)
 library(tidyr)
 setwd(here())
+
+
 df<-read.csv("data/population_over_50.csv")
-df$Males_risk=df$Males* 0.067
-df$Females_risk=df$Females* 0.219
+df$Males_risk=df$Males* 0.067 
+df$Females_risk=df$Females* 0.219 
 df_long <- gather(df, key = "Gender", value = "Risk", Females_risk,Males_risk)
+
+
+
 library(tidyr)
+
 df_long <- gather(df, key = "Gender", value = "Risk", Males_risk, Females_risk)
+
 png("output/osteoporosis_population_2018_2022.png")
 ggplot(df_long, aes(x = years, y = Risk, fill = Gender)) +
-geom_bar(stat = "identity", position = "stack") +
-labs(title = "Male and Female Estimated osteoporosis populations Over the Years",
-x = "Year",
-y = "Population at Risk") +
-scale_fill_manual(values = c("pink", "blue"),
-labels = c("Female", "Male")) +
-theme_minimal()
+  geom_bar(stat = "identity", position = "stack") +
+  labs(title = "Male and Female Estimated osteoporosis populations Over the Years",
+       x = "Year",
+       y = "Population at Risk") +
+  scale_fill_manual(values = c("pink", "blue"),  
+                    labels = c("Female", "Male")) +  
+  theme_minimal()
+
 dev.off()
