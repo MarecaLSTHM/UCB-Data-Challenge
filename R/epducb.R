1c0bd98a54df1564738e342cb7f7c39ab1ef35ed
Patty Lu <pattythlu@gmail.com>
2024-01-22 18:28:24 +0000

    per capita calculation

--- R/epducb.R
+++ R/epducb.R
@@ -445,5 +445,95 @@ region_case <- data.frame(
   proportion_of_50_M = c(M_prop_EastofEngland, M_prop_London, M_prop_mid, M_prop_NE_Y, M_prop_NorthWest, M_prop_SouthEast, M_prop_SouthWest)
 )
 
+# In Per Capita
+
+# Regional Level 
+# Load Dataset
+region_pop <- read.csv('data/Regional_pop.csv')
+
+## 1. East
+case_east_F <- 0.219 * sum(region_pop$Females[region_pop$Region == 'East' & region_pop$Age >= 50])
+case_east_M <- 0.067 * sum(region_pop$Males[region_pop$Region == 'East' & region_pop$Age >= 50])
+total_case_east <- case_east_F + case_east_M 
+total_pop_east_F <- sum(region_pop$Females[region_pop$Region == 'East'])
+total_pop_east_M <- sum(region_pop$Males[region_pop$Region == 'East'])  
+total_pop_east <- total_pop_east_F + total_pop_east_M
+east_F_pc <- round(total_pop_east_F/total_pop_east*100, 2)
+east_M_pc <- round(total_pop_east_M/total_pop_east*100, 2)
+
+## 2. London 
+case_london_F <- 0.219 * sum(region_pop$Females[region_pop$Region == 'London' & region_pop$Age >= 50])
+case_london_M <- 0.067 * sum(region_pop$Males[region_pop$Region == 'London' & region_pop$Age >= 50]) 
+total_case_london <- case_london_F + case_london_M 
+total_pop_london_F <- sum(region_pop$Females[region_pop$Region == 'London'])
+total_pop_london_M <- sum(region_pop$Males[region_pop$Region == 'London'])  
+total_pop_london <- total_pop_london_F + total_pop_london_M
+london_F_pc <- round(total_pop_london_F/total_pop_london*100, 2)
+london_M_pc <- round(total_pop_london_M/total_pop_london*100, 2)
+
+## 3. Midlands (East AND West Midlands)
+case_midlands_F <- 0.219 * sum(region_pop$Females[region_pop$Region == 'East Midlands' & region_pop$Age >= 50]) + 
+  0.219 * sum(region_pop$Females[region_pop$Region == 'West Midlands' & region_pop$Age >= 50])
+case_midlands_M <- 0.067 * sum(region_pop$Males[region_pop$Region == 'East Midlands' & region_pop$Age >= 50]) + 
+  0.067 * sum(region_pop$Males[region_pop$Region == 'West Midlands' & region_pop$Age >= 50])
+total_case_midlands <- case_midlands_F + case_midlands_M
+total_pop_mid_F <- sum(region_pop$Females[region_pop$Region == 'East Midlands']) + sum(region_pop$Females[region_pop$Region == 'West Midlands'])
+total_pop_mid_M <- sum(region_pop$Males[region_pop$Region == 'East Midlands'])  + sum(region_pop$Males[region_pop$Region == 'West Midlands'])
+total_pop_mid <- total_pop_mid_F + total_pop_mid_M
+mid_F_pc <- round(total_pop_mid_F/total_pop_mid*100, 2)
+mid_M_pc <- round(total_pop_mid_M/total_pop_mid*100, 2)
+
+## 4. North East AND Yorkshire
+case_northeast_yorkshire_F <- 0.219 * sum(region_pop$Females[region_pop$Region == 'Yorkshire & the Humber' & region_pop$Age >= 50]) + 
+  0.219 * sum(region_pop$Females[region_pop$Region == 'Northeast' & region_pop$Age >= 50])
+case_northeast_yorkshire_M <- 0.067 * sum(region_pop$Males[region_pop$Region == 'Yorkshire & the Humber' & region_pop$Age >= 50]) + 
+  0.067 * sum(region_pop$Males[region_pop$Region == 'Northeast' & region_pop$Age >= 50])
+total_case_northeast_yorkshire <- case_northeast_yorkshire_F + case_northeast_yorkshire_M
+total_pop_northeast_yorkshire_F <- sum(region_pop$Females[region_pop$Region == 'Yorkshire & the Humber']) + sum(region_pop$Females[region_pop$Region == 'Northeast'])
+total_pop_northeast_yorkshire_M <- sum(region_pop$Males[region_pop$Region == 'Yorkshire & the Humber']) + sum(region_pop$Males[region_pop$Region == 'Northeast'])
+total_pop_northeast_yorkshire <- total_pop_northeast_yorkshire_F + total_pop_northeast_yorkshire_M
+ny_F_pc <- round(total_pop_northeast_yorkshire_F/total_pop_northeast_yorkshire*100, 2)
+ny_M_pc <- round(total_pop_northeast_yorkshire_M/total_pop_northeast_yorkshire*100, 2)
+
+## 5. North West
+case_northwest_F <- 0.219 * sum(region_pop$Females[region_pop$Region == 'North West' & region_pop$Age >= 50]) 
+case_northwest_M <- 0.067 * sum(region_pop$Males[region_pop$Region == 'North West' & region_pop$Age >= 50]) 
+total_case_northwest <- case_northwest_F + case_northwest_M 
+total_pop_northwest_F <- sum(region_pop$Females[region_pop$Region == 'North West'])
+total_pop_northwest_M <- sum(region_pop$Males[region_pop$Region == 'North West'])  
+total_pop_northwest <- total_pop_northwest_F + total_pop_northwest_M
+nw_F_pc <- round(total_pop_northwest_F/total_pop_northwest*100, 2)
+nw_M_pc <- round(total_pop_northwest_M/total_pop_northwest*100, 2)
+
+## 6. South East 
+case_southeast_F <- 0.219 * sum(region_pop$Females[region_pop$Region == 'South East' & region_pop$Age >= 50]) 
+case_southeast_M <- 0.067 * sum(region_pop$Males[region_pop$Region == 'South East' & region_pop$Age >= 50]) 
+total_case_southeast <- case_southeast_F + case_southeast_M 
+total_pop_southeast_F <- sum(region_pop$Females[region_pop$Region == 'South East'])
+total_pop_southeast_M <- sum(region_pop$Males[region_pop$Region == 'South East'])  
+total_pop_southeast <- total_pop_southeast_F + total_pop_southeast_M
+se_F_pc <- round(total_pop_southeast_F/total_pop_southeast*100, 2)
+se_M_pc <- round(total_pop_southeast_M/total_pop_southeast*100, 2)
+
+## 7. South West 
+case_southwest_F <- 0.219 * sum(region_pop$Females[region_pop$Region == 'South West' & region_pop$Age >= 50]) 
+case_southwest_M <- 0.067 * sum(region_pop$Males[region_pop$Region == 'South West' & region_pop$Age >= 50]) 
+total_case_southwest <- case_southwest_F + case_southwest_M 
+total_pop_southwest_F <- sum(region_pop$Females[region_pop$Region == 'South West'])
+total_pop_southwest_M <- sum(region_pop$Males[region_pop$Region == 'South West'])  
+total_pop_southwest <- total_pop_southwest_F + total_pop_southwest_M
+sw_F_pc <- round(total_pop_southwest_F/total_pop_southwest*100, 2)
+sw_M_pc <- round(total_pop_southwest_M/total_pop_southwest*100, 2)
+
+# Create a data frame directly
+region_case <- data.frame(
+  Region = c("East of England", "London", "Midlands", "North East and Yorkshire", "North West", "South East", "South West"),
+  case_Females = c(case_east_F, case_london_F, case_midlands_F, case_northeast_yorkshire_F, case_northwest_F, case_southeast_F, case_southwest_F),
+  case_Males = c(case_east_M, case_london_M, case_midlands_M, case_northeast_yorkshire_M, case_northwest_M, case_southeast_M, case_southwest_M),
+  Total_case = c(total_case_east, total_case_london, total_case_midlands, total_case_northeast_yorkshire, total_case_northwest, total_case_southeast, total_case_southwest),
+  per_capita_F = c(east_F_pc, london_F_pc, mid_F_pc, ny_F_pc, nw_F_pc, se_F_pc, sw_F_pc), 
+  per_capita_M = c(east_M_pc, london_M_pc, mid_M_pc, ny_M_pc, nw_M_pc, se_M_pc, sw_M_pc)
+)
+
 
 #did someone delet my dev.off()s? if you did this i will find you and kill you. 
