ce008cab62cf2a6a9117a3c6032f891a53dc9f14
Patty Lu <pattythlu@gmail.com>
2024-01-26 12:03:01 +0000

    change in plot style and themes

--- R/epducb.R
+++ R/epducb.R
@@ -292,12 +292,18 @@ dev.off()
 png(filename = "output/df_drugs_with_time_without_geography.png")
 
 # Plot Prescription Trends from 2019-2023
+library(ggthemes)
 colnames(df_drug_date) <- c('Date', 'Bisphosphonates', 'Calcium', 'Denosumab', 'Parathyroid Hormones & Analogues')
 library(data.table)
-df_drug_date_long <- melt(setDT(df_drug_date), id.vars = c("Date"), variable.name = 'medication')
-ggplot(df_drug_date_long, aes(x = Date, y = value)) + 
-  geom_line(aes(colour = medication, group = medication))+
-  theme_bw() 
+df_drug_date_long <- melt(setDT(df_drug_date), id.vars = c("Date"), variable.name = 'Medication')
+colnames(df_drug_date_long) <- c('Year', 'Medication', 'Values')
+trend_plot <- ggplot(df_drug_date_long, aes(x = Year, y = Values)) + 
+  geom_line(aes(colour = Medication, group = Medication))+
+  theme_bw()+
+  theme(text=element_text(family="Times", size=12)) #Times New Roman, 12pt, Bold 
+
+print(trend_plot+labs(y= "Number of Prescriptions"))
+
 dev.off()
 
 #Patrica this ones the lines are right but the legends are gone
